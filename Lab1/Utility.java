bfd2001ed973bee77f0ecfb5a72b52dd19737224
Артем Крылов <kav128@outlook.com>
2019-11-21 22:08:51 +0300

    Change GCD algorithm

--- Lab1/Utility.java
+++ Lab1/Utility.java
@@ -1,11 +1,14 @@
 
 public class Utility {
-	public static int GCD(int a, int b) {
-		while (a != 0 && b != 0)
-			if (a > b)
-				a %= b;
-			else
-				b %= a;
-		return a + b;
-	}
+    public static int GCD(int a, int b) {
+        a = Math.abs(a);
+        b = Math.abs(b);
+        int t;
+        while (b != 0) {
+            t = b;
+            b = a % b;
+            a = t;
+        }
+        return a;
+    }
 }
